39c75d92e2452e86c46b5271f14139ef06790f87
Ale Damilola <damidigi@yahoo.com>
2019-10-27 10:50:41 +0100

    Add Select within select quiz

--- Select_From_Nobel.sql
+++ Select_From_Nobel.sql
@@ -73,4 +73,4 @@ ORDER BY yr DESC, winner;
 SELECT winner, subject
 FROM nobel
 WHERE yr = 1984
-ORDER BY subject IN ('Physics','Chemistry'),subject,winner
+ORDER BY subject IN ('Physics','Chemistry'),subject, winner;
